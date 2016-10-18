48134d30b7c6bee0e2a26c477f4cff0cbc9fc7ae
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-18 20:27:57 +0200

    bootstrapping

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,19 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <meta name="csfr-token" content="{{ csrf_field() }}">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
 <form action="/login" method="POST">
-    <input type="text" name="user">
-    <input type="password" name="password">
-    <input type="submit" value="Login">
+    Usuari: <input type="text" name="user">
+    Password: <input type="password" name="password">
+    <input type="submit" value="Login" class="btn btn-danger">
 </form>
 </body>
 </html>
